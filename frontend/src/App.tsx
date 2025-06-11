9193384621f1d27e114b334bdce9d1dc0a22501a
Matt443 <marcin26050606@gmail.com>
2025-06-11 20:26:27 +0200

    Table component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,21 @@ import { useLazyGetAllCitiesQuery, useLazyGetCitiesQuery } from "./features/ApiS
 import { useSelector } from "react-redux";
 import { RootState } from "./stores/state.store";
 import { useEffect } from "react";
+import DataTable from "./components/DataTable.component";
+import { GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 10 },
+    { field: "name", headerName: "name", width: 200 },
+    { field: "username", headerName: "username", width: 100 },
+];
 
 function App() {
     const [loadCities, { data: cities }] = useLazyGetCitiesQuery();
     const [loadAllCities, { data: allCities }] = useLazyGetAllCitiesQuery();
+
     const inputValue = useSelector((state: RootState) => state.data.searchedFor);
+
     useEffect(() => {
         loadAllCities();
     }, [loadAllCities]);
@@ -17,13 +27,7 @@ function App() {
                 searchCallback={() => loadCities({ name: inputValue })}
                 cities={Array.isArray(allCities) ? allCities.map((item) => item.name) : []}
             ></TopBar>
-            <ul className="text-white">
-                {cities?.map((user) => (
-                    <li className="text-green" key={user.name}>
-                        {user.name}
-                    </li>
-                ))}
-            </ul>
+            <DataTable rows={cities ? cities : []} columns={columns}></DataTable>
         </>
     );
 }
---
+++ frontend/src/components/DataTable.component.tsx
@@ -0,0 +1,48 @@
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+interface DataTableProps {
+    columns: GridColDef[];
+    rows: object[];
+}
+
+const tableCSS = {
+    border: 0,
+    maxWidth: "100%",
+    "& .MuiTablePagination-root, & div.MuiDataGrid-columnHeaders .MuiDataGrid-columnHeader, & div.MuiDataGrid-columnHeaders .MuiDataGrid-filler, & .MuiDataGrid-overlay":
+        { bgcolor: "black", color: "white" },
+    "& .MuiTablePagination-root svg": { color: "white" },
+    "& div.MuiDataGrid-virtualScrollerRenderZone": {
+        bgcolor: "black",
+        color: "white",
+    },
+    "& .MuiCheckbox-root svg": {
+        color: "var(--color-green-400)",
+    },
+    "& .MuiDataGrid-row:hover, & .MuiDataGrid-row.Mui-selected": {
+        bgcolor: "var(--color-green-900)",
+    },
+    "& .MuiDataGrid-row.Mui-selected:hover": {
+        bgcolor: "var(--color-green-700)",
+    },
+};
+
+const paginationModel = { page: 0, pageSize: 5 };
+
+const DataTable: React.FC<DataTableProps> = ({ columns, rows }) => {
+    console.log(rows);
+    return (
+        <>
+            <DataGrid
+                sx={tableCSS}
+                rows={rows}
+                columns={columns}
+                initialState={{ pagination: { paginationModel } }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+                // onRowSelectionModelChange={(newSelection) => {console.log(newSelection)}}
+            />
+        </>
+    );
+};
+
+export default DataTable;
